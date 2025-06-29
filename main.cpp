7716f84de061a4305ce2e96ff52b2739b7c742e3
Aron Tinkl <arontinkl@gmail.com>
2025-06-29 21:13:12 +0200

    feat: added bottom menu and refactored some code

--- main.cpp
+++ main.cpp
@@ -1,88 +1,100 @@
 #include <ncurses.h>
-bool valid_coordinates(WINDOW* win, int step_x, int step_y, int player_x, int player_y);
+#include <vector>
+bool valid_coordinates(WINDOW *win, int step_x, int step_y, int player_x,
+                       int player_y);
 
 int main() {
+
+  //NCURSES INIT
   initscr();
   cbreak();
   noecho();
-  int c=0;
+
+  // GENERAL SCREEN
   int rows, cols;
   getmaxyx(stdscr, rows, cols);
-  mvprintw(rows - 1, 0, "Press 'q' to exit... ");
-int win_height=30; 
-  int win_width=30; 
-  int start_y= (rows-win_height)/2; 
-  int start_x= (cols- win_width)/2; 
-  WINDOW* win = newwin(win_height, win_width, start_y, start_x);
+
+  // BOTTOM MENU
+  int bottom_menu_height=5; 
+  int bottom_menu_width=cols;
+  WINDOW *bottom_menu= newwin(bottom_menu_height, bottom_menu_width, rows-bottom_menu_height,0);
+  box(bottom_menu, 0,0);
+  mvwprintw(bottom_menu, 1, 1, "Press 'q' to exit... ");
+
+  // GRID
+  int grid_height = 30;
+  int grid_width = 30;
+  int grid_y = (rows - grid_height) / 2;
+  int grid_x = (cols - grid_width) / 2;
+  WINDOW *grid= newwin(grid_height, grid_width, grid_y, grid_x);
+  keypad(grid, TRUE);
+  box(grid, 0, 0);
+  wmove(grid, 10, 1);
+  whline(grid, ACS_HLINE, grid_width - 2);
+  wmove(grid, 20, 1);
+  whline(grid, ACS_HLINE, grid_width - 2);
+  wmove(grid, 1, 10);
+  wvline(grid, ACS_VLINE, grid_height - 2);
+  wmove(grid, 1, 20);
+  wvline(grid, ACS_VLINE, grid_height - 2);
   refresh();
-  keypad(win, TRUE);
-  box(win, 0, 0);
-  wmove(win, 10,1); 
-  whline(win,ACS_HLINE, win_width-2);
-  wmove(win,20,1);
-  whline(win,ACS_HLINE, win_width-2);
-  wmove(win,1,10);
-  wvline(win,ACS_VLINE, win_height-2);
-  wmove(win,1,20);
-  wvline(win,ACS_VLINE, win_height-2);
-  int player_x=15;
-  int player_y=15; 
-  wmove(win, player_x, player_y);
+
+  // PLAYER INIT
+  int player_start_y=15; 
+  int player_start_x=15; 
+  int player_y = player_start_y;
+  int player_x = player_start_x;
+  wmove(grid, player_y, player_x);
+
+  // INPUT PARSE
+  int c = 0;
   while (c != 'q') {
-    c = wgetch(win); 
+    c = wgetch(grid);
+
+    mvwprintw(grid, player_y, player_x, " ");
+
     switch (c) {
-      case KEY_DOWN:
-        if(!valid_coordinates(win, 0, 10, player_x, player_y)){
-            mvprintw(rows -2, 0, "Not a valid move!");
-        }else{
-        player_y=player_y+10;
-        wmove(win, player_y, player_x);
-        }
-        break;
-      case KEY_UP:
-        if(!valid_coordinates(win, 0, -10, player_x, player_y)){
-            mvprintw(rows -2, 0, "Not a valid move!");
-        }else{
-        player_y=player_y-10;
-        wmove(win, player_y, player_x); 
-        }
-        break;
-      case KEY_RIGHT:
-        if(!valid_coordinates(win, 10, 0, player_x, player_y)){
-            mvprintw(rows -2, 0, "Not a valid move!");
-        }else{
-        player_x=player_x+10;
-        wmove(win, player_y, player_x); 
-        }
-        break;
-      case KEY_LEFT:
-        if(!valid_coordinates(win, -10, 0, player_x, player_y)){
-            mvprintw(rows -2, 0, "Not a valid move!");
-        }else{
-        player_x=player_x-10;
-        wmove(win, player_y, player_x); 
-        }
-        break;
-      case 'q':
-        break;
-      default:
-        mvprintw(rows - 2, 0, "No matching key found!");
-        break;
+    case KEY_DOWN:
+      if (valid_coordinates(grid, 1, 0, player_y, player_x)) player_y += 10;
+      else mvwprintw(bottom_menu, 3, 1, "Not a valid move!");
+      break;
+    case KEY_UP:
+      if (valid_coordinates(grid, -10, 0, player_y, player_x)) player_y -= 10;
+      else mvwprintw(bottom_menu, 3, 1, "Not a valid move!");
+      break;
+    case KEY_RIGHT:
+      if (valid_coordinates(grid, 0, 10, player_y, player_x)) player_x += 10;
+      else mvwprintw(bottom_menu, 3, 1, "Not a valid move!");
+      break;
+    case KEY_LEFT:
+      if (valid_coordinates(grid, 0, -10, player_y, player_x)) player_x -= 10;
+      else mvwprintw(bottom_menu, 3, 1, "Not a valid move!");
+      break;
+    default:
+      mvwprintw(bottom_menu, 3, 1, "No matching key found! ");
+      break;
     }
-    refresh();     
-    wrefresh(win); 
+
+    mvwprintw(grid, player_y, player_x, "X");
+
+    // REFRESH
+    refresh();
+    wrefresh(grid);
+    wrefresh(bottom_menu);
   }
 
   endwin();
   return 0;
 }
-bool valid_coordinates(WINDOW* win, int step_x, int step_y, int player_x, int player_y) {
-    int rows, cols; 
-    getmaxyx(win, rows, cols);
 
-    int new_x = player_x + step_x;
-    int new_y = player_y + step_y;
+// Check if step into direction step_y, step_x are within grid bounds of grid for player_y, player_x
+bool valid_coordinates(WINDOW *grid, int step_y, int step_x, int player_y,
+                       int player_x) {
+  int rows, cols;
+  getmaxyx(grid, rows, cols);
 
-    return (new_x >= 0 && new_x < cols) && (new_y >= 0 && new_y < rows);
+  int new_y = player_y + step_y;
+  int new_x = player_x + step_x;
+
+  return (new_y >= 0 && new_y < rows) && (new_x >= 0 && new_x < cols);
 }
-
